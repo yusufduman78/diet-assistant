93c207a0fa117129bbfe787215d083375e51babc
smoke7806 <yusuf78duman@gmail.com>
2025-09-05 01:43:58 +0300

    all folder structure is compatible with code

--- data/dataset_edit/merge_sets.py
+++ data/dataset_edit/merge_sets.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import pandas as pd
+from tqdm import tqdm
 
 raw_dir = "../raw"
 mapping_file = "../metadata/class_mapping.csv"
@@ -10,32 +11,41 @@ mapping_pd = pd.read_csv(mapping_file)
 
 os.makedirs(processed_dir, exist_ok=True)
 
-for _,row in mapping_pd.iterrows():
+def find_class(base_path,target_class):
+
+    for root,dirs,files in os.walk(base_path):
+        if os.path.basename(root) == target_class:
+            return root
+    return None
+
+for _,row in tqdm(mapping_pd.iterrows(), total = len(mapping_pd), desc="Processing"):
     last_class = row["last_class"]
     current_class = row["current_class"]
     dataset = row["dataset"]
 
-    original_dir = os.path.join(raw_dir,dataset,current_class)
-    final_dir = os.path.join(processed_dir,last_class)
+    dataset_dir = os.path.join(raw_dir,dataset)
+    class_dir = find_class(dataset_dir,current_class)
 
-    os.makedirs(final_dir, exist_ok=True)
-
-    if not os.path.exists(original_dir):
-        print(f" {original_dir} not found. Skipping...")
+    if not class_dir:
+        print(f"{current_class} folder not found in {dataset}")
         continue
 
-    for jpg_file in os.listdir(original_dir):
-        original_path = os.path.join(original_dir, jpg_file)
-        final_path = os.path.join(final_dir, jpg_file)
+    final_dir = os.path.join(processed_dir,last_class)
+    os.makedirs(final_dir, exist_ok=True)
 
-        if os.path.exists(final_path):
-            first,second = os.path.splitext(jpg_file)
-            i = 1
-            while os.path.exists(final_path):
-                final_path = os.path.join(final_dir, f"{first}_{i}{second}")
-                i += 1
+    file_list = os.listdir(class_dir)
 
-        shutil.copy2(original_path,final_path)
+    for jpg_file in tqdm(file_list,desc=f"Copying folder '{current_class}'"):
+        if jpg_file.lower().endswith((".jpg",".png")):
+            start_path = os.path.join(class_dir,jpg_file)
+            final_path = os.path.join(final_dir,jpg_file)
 
-print(f"'processed' folder was done.")
+            if os.path.exists(final_path):
+                fst, snd = os.path.splitext(jpg_file)
+                i = 1
+                while os.path.exists(final_path):
+                    final_path = os.path.join(final_dir, f"{fst}_{i}{snd}")
+                    i += 1
+            shutil.copy2(start_path,final_path)
 
+print(f"'processed' was done.")
